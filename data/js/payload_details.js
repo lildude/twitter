bd389745771b42dbe8e74d01a881b28d3ff9d8ea
Colin Seymour <colin@github.com>
2017-01-04 01:19:08 +0000

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4877,
-	"created_at": "Tue Jan 03 01:19:06 +0000 2017",
+	"tweets": 4879,
+	"created_at": "Wed Jan 04 01:19:07 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_01.js
+++ data/js/tweets/2017_01.js
@@ -1,6 +1,464 @@
 Grailbird.data.tweets_2017_01 = 
 [
 	{
+		"created_at": "Tue Jan 03 10:09:13 +0000 2017",
+		"id": 816224890265206800,
+		"id_str": "816224890265206784",
+		"text": "RT @ellenbacca: ARE YOU KIDDING ME THIS IS THE COOLEST!! https://t.co/xrbMdf9dkp",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ellenbacca",
+					"name": "Ellen Bacca",
+					"id": 552444185,
+					"id_str": "552444185",
+					"indices": [
+						3,
+						14
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 815698413123752000,
+					"id_str": "815698413123751936",
+					"indices": [
+						57,
+						80
+					],
+					"media_url": "http://pbs.twimg.com/media/C1HxYo4XUAAeHwI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C1HxYo4XUAAeHwI.jpg",
+					"url": "https://t.co/xrbMdf9dkp",
+					"display_url": "pic.twitter.com/xrbMdf9dkp",
+					"expanded_url": "https://twitter.com/ellenbacca/status/815698418823811072/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 635,
+							"h": 664,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 635,
+							"h": 664,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 635,
+							"h": 664,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 815698418823811100,
+					"source_status_id_str": "815698418823811072",
+					"source_user_id": 552444185,
+					"source_user_id_str": "552444185"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 815698413123752000,
+					"id_str": "815698413123751936",
+					"indices": [
+						57,
+						80
+					],
+					"media_url": "http://pbs.twimg.com/media/C1HxYo4XUAAeHwI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C1HxYo4XUAAeHwI.jpg",
+					"url": "https://t.co/xrbMdf9dkp",
+					"display_url": "pic.twitter.com/xrbMdf9dkp",
+					"expanded_url": "https://twitter.com/ellenbacca/status/815698418823811072/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 635,
+							"h": 664,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 635,
+							"h": 664,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 635,
+							"h": 664,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 815698418823811100,
+					"source_status_id_str": "815698418823811072",
+					"source_user_id": 552444185,
+					"source_user_id_str": "552444185"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 8812362,
+			"id_str": "8812362",
+			"name": "lildude",
+			"screen_name": "lildude",
+			"location": "Everywhere. Wha ha ha ha haaa!",
+			"description": "GitHub developing, beer drinking, barefoot & minimalist running fundi.",
+			"url": "https://t.co/3MoMsEOqye",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3MoMsEOqye",
+							"expanded_url": "https://lildude.co.uk",
+							"display_url": "lildude.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 467,
+			"friends_count": 269,
+			"listed_count": 18,
+			"created_at": "Tue Sep 11 15:19:08 +0000 2007",
+			"favourites_count": 633,
+			"utc_offset": 0,
+			"time_zone": "London",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4879,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000841109441/5aec21ab2289989e0f184dfd7ef4d0e2_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000841109441/5aec21ab2289989e0f184dfd7ef4d0e2_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/8812362/1478998839",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sun Jan 01 23:17:12 +0000 2017",
+			"id": 815698418823811100,
+			"id_str": "815698418823811072",
+			"text": "ARE YOU KIDDING ME THIS IS THE COOLEST!! https://t.co/xrbMdf9dkp",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 815698413123752000,
+						"id_str": "815698413123751936",
+						"indices": [
+							41,
+							64
+						],
+						"media_url": "http://pbs.twimg.com/media/C1HxYo4XUAAeHwI.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C1HxYo4XUAAeHwI.jpg",
+						"url": "https://t.co/xrbMdf9dkp",
+						"display_url": "pic.twitter.com/xrbMdf9dkp",
+						"expanded_url": "https://twitter.com/ellenbacca/status/815698418823811072/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 635,
+								"h": 664,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 635,
+								"h": 664,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 635,
+								"h": 664,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 815698413123752000,
+						"id_str": "815698413123751936",
+						"indices": [
+							41,
+							64
+						],
+						"media_url": "http://pbs.twimg.com/media/C1HxYo4XUAAeHwI.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C1HxYo4XUAAeHwI.jpg",
+						"url": "https://t.co/xrbMdf9dkp",
+						"display_url": "pic.twitter.com/xrbMdf9dkp",
+						"expanded_url": "https://twitter.com/ellenbacca/status/815698418823811072/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 635,
+								"h": 664,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 635,
+								"h": 664,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 635,
+								"h": 664,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 552444185,
+				"id_str": "552444185",
+				"name": "Ellen Bacca",
+				"screen_name": "ellenbacca",
+				"location": "",
+				"description": "WOOD TV Certified Meteorologist #NWA #CBM Emmy nominated, Adjunct Prof, Marathoner, Optimist, Christian, Wife, Valpo Alumn. Dark roasts|Good books ☕️",
+				"url": "https://t.co/8GITHt2c9k",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/8GITHt2c9k",
+								"expanded_url": "http://ellenbacca.com",
+								"display_url": "ellenbacca.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3215,
+				"friends_count": 1006,
+				"listed_count": 98,
+				"created_at": "Fri Apr 13 05:25:06 +0000 2012",
+				"favourites_count": 8365,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 9624,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "1C5775",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/566787154030772225/0LIrNhPi.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/566787154030772225/0LIrNhPi.jpeg",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/802488467133530112/qKzA3Mfa_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/802488467133530112/qKzA3Mfa_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/552444185/1458427097",
+				"profile_link_color": "3B94D9",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1245,
+			"favorite_count": 3731,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1245,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Tue Jan 03 08:33:44 +0000 2017",
+		"id": 816200862511071200,
+		"id_str": "816200862511071232",
+		"text": "https://t.co/JvA18VsDlk",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/JvA18VsDlk",
+					"expanded_url": "https://www.bloomberg.com/news/articles/2017-01-03/for-cheapest-power-on-earth-look-skyward-as-coal-falls-to-solar",
+					"display_url": "bloomberg.com/news/articles/…",
+					"indices": [
+						0,
+						23
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 8812362,
+			"id_str": "8812362",
+			"name": "lildude",
+			"screen_name": "lildude",
+			"location": "Everywhere. Wha ha ha ha haaa!",
+			"description": "GitHub developing, beer drinking, barefoot & minimalist running fundi.",
+			"url": "https://t.co/3MoMsEOqye",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3MoMsEOqye",
+							"expanded_url": "https://lildude.co.uk",
+							"display_url": "lildude.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 467,
+			"friends_count": 269,
+			"listed_count": 18,
+			"created_at": "Tue Sep 11 15:19:08 +0000 2007",
+			"favourites_count": 633,
+			"utc_offset": 0,
+			"time_zone": "London",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4879,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000841109441/5aec21ab2289989e0f184dfd7ef4d0e2_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000841109441/5aec21ab2289989e0f184dfd7ef4d0e2_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/8812362/1478998839",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und"
+	},
+	{
 		"created_at": "Mon Jan 02 11:35:45 +0000 2017",
 		"id": 815884280626868200,
 		"id_str": "815884280626868224",
