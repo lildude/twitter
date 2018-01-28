328b847615f40ad93518c7a6adc8e2379ce08dd1
Colin Seymour <colin@github.com>
2018-01-28 01:19:07 +0000

    Added 1 tweets

--- data/js/tweets/2018_01.js
+++ data/js/tweets/2018_01.js
@@ -1,6 +1,245 @@
 Grailbird.data.tweets_2018_01 = 
 [
 	{
+		"created_at": "Sat Jan 27 11:14:52 +0000 2018",
+		"id": 957210291237998600,
+		"id_str": "957210291237998592",
+		"text": "RT @ComradesRace: Hot off the press! The official #Comrades2018 route distance has been  confirmed by Race Director @JamesRowyn as 90.184km…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Comrades2018",
+					"indices": [
+						50,
+						63
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ComradesRace",
+					"name": "Comrades Marathon",
+					"id": 255402231,
+					"id_str": "255402231",
+					"indices": [
+						3,
+						16
+					]
+				},
+				{
+					"screen_name": "JamesRowyn",
+					"name": "Rowyn James",
+					"id": 397496731,
+					"id_str": "397496731",
+					"indices": [
+						116,
+						127
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 8812362,
+			"id_str": "8812362",
+			"name": "Lildude Esquire 🤘",
+			"screen_name": "lildude",
+			"location": "Everywhere. Wha ha ha ha haaa!",
+			"description": "GitHub developing, beer drinking, barefoot & minimalist running fundi and stay-at-home astronaut.",
+			"url": "https://t.co/3MoMsEOqye",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3MoMsEOqye",
+							"expanded_url": "https://lildude.co.uk",
+							"display_url": "lildude.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 503,
+			"friends_count": 279,
+			"listed_count": 19,
+			"created_at": "Tue Sep 11 15:19:08 +0000 2007",
+			"favourites_count": 731,
+			"utc_offset": 0,
+			"time_zone": "London",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5238,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000841109441/5aec21ab2289989e0f184dfd7ef4d0e2_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000841109441/5aec21ab2289989e0f184dfd7ef4d0e2_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/8812362/1496944532",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Jan 26 07:13:45 +0000 2018",
+			"id": 956787226557501400,
+			"id_str": "956787226557501440",
+			"text": "Hot off the press! The official #Comrades2018 route distance has been  confirmed by Race Director @JamesRowyn as 90… https://t.co/wCupy73uJC",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "Comrades2018",
+						"indices": [
+							32,
+							45
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "JamesRowyn",
+						"name": "Rowyn James",
+						"id": 397496731,
+						"id_str": "397496731",
+						"indices": [
+							98,
+							109
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/wCupy73uJC",
+						"expanded_url": "https://twitter.com/i/web/status/956787226557501440",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							117,
+							140
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 255402231,
+				"id_str": "255402231",
+				"name": "Comrades Marathon",
+				"screen_name": "ComradesRace",
+				"location": "Pietermaritzburg, South Africa",
+				"description": "The official Comrades Marathon Twitter account. The Ultimate Human Race is the world's oldest and largest ultramarathon.",
+				"url": "https://t.co/FFUhA4nlTM",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/FFUhA4nlTM",
+								"expanded_url": "http://www.comrades.com",
+								"display_url": "comrades.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 41707,
+				"friends_count": 11738,
+				"listed_count": 290,
+				"created_at": "Mon Feb 21 08:41:58 +0000 2011",
+				"favourites_count": 17268,
+				"utc_offset": 7200,
+				"time_zone": "Pretoria",
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 24010,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "E2DC4C",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/378800000065905547/68876911b75bc2ab7c32b1841abdf206.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/378800000065905547/68876911b75bc2ab7c32b1841abdf206.jpeg",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/631723196845305856/gsafmCoC_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/631723196845305856/gsafmCoC_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/255402231/1515403631",
+				"profile_link_color": "F7AA06",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "5B585D",
+				"profile_text_color": "EEE235",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 40,
+			"favorite_count": 41,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 40,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en"
+	},
+	{
 		"created_at": "Fri Jan 26 18:54:29 +0000 2018",
 		"id": 956963572352876500,
 		"id_str": "956963572352876545",
