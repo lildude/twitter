b643b38ece612a88d056b9ffae8cc1469138d3db
Colin Seymour <colin@github.com>
2018-06-10 01:19:06 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5358,
-	"created_at": "Sat Jun 09 00:19:05 +0000 2018",
+	"tweets": 5360,
+	"created_at": "Sun Jun 10 00:19:05 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_06.js
+++ data/js/tweets/2018_06.js
@@ -1,6 +1,497 @@
 Grailbird.data.tweets_2018_06 = 
 [
 	{
+		"created_at": "Sat Jun 09 23:23:50 +0000 2018",
+		"id": 1005591330200981500,
+		"id_str": "1005591330200981504",
+		"text": "Sssh 🤫🤫, don’t tell anyone but today’s the day. (Up just after going to bed to catch the coach to the start) #Comrades2018",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Comrades2018",
+					"indices": [
+						109,
+						122
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 8812362,
+			"id_str": "8812362",
+			"name": "Lildude Esquire 🤘",
+			"screen_name": "lildude",
+			"location": "Everywhere. Wha ha ha ha haaa!",
+			"description": "GitHub developing, beer drinking, barefoot & minimalist running fundi and stay-at-home astronaut. Run stuffs at https://t.co/OlEtj7TstY",
+			"url": "https://t.co/3MoMsEOqye",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3MoMsEOqye",
+							"expanded_url": "https://lildude.co.uk",
+							"display_url": "lildude.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/OlEtj7TstY",
+							"expanded_url": "http://gonefora.run",
+							"display_url": "gonefora.run",
+							"indices": [
+								112,
+								135
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 513,
+			"friends_count": 282,
+			"listed_count": 18,
+			"created_at": "Tue Sep 11 15:19:08 +0000 2007",
+			"favourites_count": 810,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5331,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/964993178230288384/vi8iEvhL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/964993178230288384/vi8iEvhL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/8812362/1524488467",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Sat Jun 09 05:13:31 +0000 2018",
+		"id": 1005316943082147800,
+		"id_str": "1005316943082147840",
+		"text": "RT @ComradesRace: 1 day, 20 hours. #Comrades2018 https://t.co/brZIzRl0NS",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Comrades2018",
+					"indices": [
+						35,
+						48
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ComradesRace",
+					"name": "Comrades Marathon",
+					"id": 255402231,
+					"id_str": "255402231",
+					"indices": [
+						3,
+						16
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1004982668046557200,
+					"id_str": "1004982668046557184",
+					"indices": [
+						49,
+						72
+					],
+					"media_url": "http://pbs.twimg.com/media/DfJqaXZW4AAMw5J.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DfJqaXZW4AAMw5J.jpg",
+					"url": "https://t.co/brZIzRl0NS",
+					"display_url": "pic.twitter.com/brZIzRl0NS",
+					"expanded_url": "https://twitter.com/ComradesRace/status/1004983179877462016/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 358,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 801,
+							"h": 422,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 801,
+							"h": 422,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1004983179877462000,
+					"source_status_id_str": "1004983179877462016",
+					"source_user_id": 255402231,
+					"source_user_id_str": "255402231"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1004982668046557200,
+					"id_str": "1004982668046557184",
+					"indices": [
+						49,
+						72
+					],
+					"media_url": "http://pbs.twimg.com/media/DfJqaXZW4AAMw5J.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DfJqaXZW4AAMw5J.jpg",
+					"url": "https://t.co/brZIzRl0NS",
+					"display_url": "pic.twitter.com/brZIzRl0NS",
+					"expanded_url": "https://twitter.com/ComradesRace/status/1004983179877462016/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 358,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 801,
+							"h": 422,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 801,
+							"h": 422,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1004983179877462000,
+					"source_status_id_str": "1004983179877462016",
+					"source_user_id": 255402231,
+					"source_user_id_str": "255402231"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 8812362,
+			"id_str": "8812362",
+			"name": "Lildude Esquire 🤘",
+			"screen_name": "lildude",
+			"location": "Everywhere. Wha ha ha ha haaa!",
+			"description": "GitHub developing, beer drinking, barefoot & minimalist running fundi and stay-at-home astronaut. Run stuffs at https://t.co/OlEtj7TstY",
+			"url": "https://t.co/3MoMsEOqye",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3MoMsEOqye",
+							"expanded_url": "https://lildude.co.uk",
+							"display_url": "lildude.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/OlEtj7TstY",
+							"expanded_url": "http://gonefora.run",
+							"display_url": "gonefora.run",
+							"indices": [
+								112,
+								135
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 513,
+			"friends_count": 282,
+			"listed_count": 18,
+			"created_at": "Tue Sep 11 15:19:08 +0000 2007",
+			"favourites_count": 810,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5331,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/964993178230288384/vi8iEvhL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/964993178230288384/vi8iEvhL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/8812362/1524488467",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Jun 08 07:07:16 +0000 2018",
+			"id": 1004983179877462000,
+			"id_str": "1004983179877462016",
+			"text": "1 day, 20 hours. #Comrades2018 https://t.co/brZIzRl0NS",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "Comrades2018",
+						"indices": [
+							17,
+							30
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1004982668046557200,
+						"id_str": "1004982668046557184",
+						"indices": [
+							31,
+							54
+						],
+						"media_url": "http://pbs.twimg.com/media/DfJqaXZW4AAMw5J.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DfJqaXZW4AAMw5J.jpg",
+						"url": "https://t.co/brZIzRl0NS",
+						"display_url": "pic.twitter.com/brZIzRl0NS",
+						"expanded_url": "https://twitter.com/ComradesRace/status/1004983179877462016/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 358,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 801,
+								"h": 422,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 801,
+								"h": 422,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1004982668046557200,
+						"id_str": "1004982668046557184",
+						"indices": [
+							31,
+							54
+						],
+						"media_url": "http://pbs.twimg.com/media/DfJqaXZW4AAMw5J.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DfJqaXZW4AAMw5J.jpg",
+						"url": "https://t.co/brZIzRl0NS",
+						"display_url": "pic.twitter.com/brZIzRl0NS",
+						"expanded_url": "https://twitter.com/ComradesRace/status/1004983179877462016/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 358,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 801,
+								"h": 422,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 801,
+								"h": 422,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 255402231,
+				"id_str": "255402231",
+				"name": "Comrades Marathon",
+				"screen_name": "ComradesRace",
+				"location": "Pietermaritzburg, South Africa",
+				"description": "The official Comrades Marathon Twitter account. The Ultimate Human Race is the world's oldest and largest ultramarathon.",
+				"url": "https://t.co/FFUhA4nlTM",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/FFUhA4nlTM",
+								"expanded_url": "http://www.comrades.com",
+								"display_url": "comrades.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 45387,
+				"friends_count": 11695,
+				"listed_count": 295,
+				"created_at": "Mon Feb 21 08:41:58 +0000 2011",
+				"favourites_count": 20343,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 25480,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "E2DC4C",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/631723196845305856/gsafmCoC_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/631723196845305856/gsafmCoC_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/255402231/1515403631",
+				"profile_link_color": "F7AA06",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "5B585D",
+				"profile_text_color": "EEE235",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 70,
+			"favorite_count": 146,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 70,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Fri Jun 08 12:09:40 +0000 2018",
 		"id": 1005059280494563300,
 		"id_str": "1005059280494563328",
