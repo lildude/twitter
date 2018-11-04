45b75a73f78631488047c3d5cb1c07ae2a20a957
Colin Seymour <colin@github.com>
2018-11-04 01:19:05 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5467,
-	"created_at": "Sat Nov 03 01:19:04 +0000 2018",
+	"tweets": 5468,
+	"created_at": "Sun Nov 04 01:19:04 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,470 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Sat Nov 03 18:36:11 +0000 2018",
+		"id": 1058789957861556200,
+		"id_str": "1058789957861556226",
+		"text": "RT @cutmaker: To all my middle aged mates in Lycra: OKAY - this IS impressive 😳 https://t.co/t3CuF6eSL5",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "cutmaker",
+					"name": "Cutmaker",
+					"id": 18370802,
+					"id_str": "18370802",
+					"indices": [
+						3,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1058338520102850600,
+					"id_str": "1058338520102850560",
+					"indices": [
+						80,
+						103
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1058338520102850560/pu/img/dDjuC_EERMkT-UEU.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1058338520102850560/pu/img/dDjuC_EERMkT-UEU.jpg",
+					"url": "https://t.co/t3CuF6eSL5",
+					"display_url": "pic.twitter.com/t3CuF6eSL5",
+					"expanded_url": "https://twitter.com/cutmaker/status/1058338915969581056/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1058338915969581000,
+					"source_status_id_str": "1058338915969581056",
+					"source_user_id": 18370802,
+					"source_user_id_str": "18370802"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1058338520102850600,
+					"id_str": "1058338520102850560",
+					"indices": [
+						80,
+						103
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1058338520102850560/pu/img/dDjuC_EERMkT-UEU.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1058338520102850560/pu/img/dDjuC_EERMkT-UEU.jpg",
+					"url": "https://t.co/t3CuF6eSL5",
+					"display_url": "pic.twitter.com/t3CuF6eSL5",
+					"expanded_url": "https://twitter.com/cutmaker/status/1058338915969581056/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1058338915969581000,
+					"source_status_id_str": "1058338915969581056",
+					"source_user_id": 18370802,
+					"source_user_id_str": "18370802",
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 85543,
+						"variants": [
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1058338520102850560/pu/vid/720x720/Gea1cPEnyrLWMN6d.mp4?tag=5"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1058338520102850560/pu/pl/jiVGJl4vGziGZ6zP.m3u8?tag=5"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1058338520102850560/pu/vid/480x480/Y8OAmUSBVUJJ3CV2.mp4?tag=5"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1058338520102850560/pu/vid/240x240/3LsC5av81ERqyH4P.mp4?tag=5"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false,
+						"source_user": {
+							"id": 18370802,
+							"id_str": "18370802",
+							"name": "Cutmaker",
+							"screen_name": "cutmaker",
+							"location": "",
+							"description": "",
+							"url": null,
+							"entities": {
+								"description": {
+									"urls": []
+								}
+							},
+							"protected": false,
+							"followers_count": 14446,
+							"friends_count": 334,
+							"listed_count": 40,
+							"created_at": "Thu Dec 25 05:03:04 +0000 2008",
+							"favourites_count": 62,
+							"utc_offset": null,
+							"time_zone": null,
+							"geo_enabled": true,
+							"verified": false,
+							"statuses_count": 5335,
+							"lang": "en-gb",
+							"contributors_enabled": false,
+							"is_translator": false,
+							"is_translation_enabled": false,
+							"profile_background_color": "C0DEED",
+							"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+							"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+							"profile_background_tile": false,
+							"profile_image_url": "http://pbs.twimg.com/profile_images/885185029734879232/jVIs5T_s_normal.jpg",
+							"profile_image_url_https": "https://pbs.twimg.com/profile_images/885185029734879232/jVIs5T_s_normal.jpg",
+							"profile_banner_url": "https://pbs.twimg.com/profile_banners/18370802/1472018469",
+							"profile_link_color": "1DA1F2",
+							"profile_sidebar_border_color": "C0DEED",
+							"profile_sidebar_fill_color": "DDEEF6",
+							"profile_text_color": "333333",
+							"profile_use_background_image": true,
+							"has_extended_profile": false,
+							"default_profile": true,
+							"default_profile_image": false,
+							"following": false,
+							"follow_request_sent": false,
+							"notifications": false,
+							"translator_type": "none"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 8812362,
+			"id_str": "8812362",
+			"name": "Lildude Esquire 🤘at 🏡",
+			"screen_name": "lildude",
+			"location": "Everywhere. Wha ha ha ha haaa!",
+			"description": "GitHub developing, beer drinking, running fundi and stay-at-home astronaut. Run stuffs at https://t.co/OlEtj7TstY",
+			"url": "https://t.co/3MoMsEOqye",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3MoMsEOqye",
+							"expanded_url": "https://lildude.co.uk",
+							"display_url": "lildude.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/OlEtj7TstY",
+							"expanded_url": "http://gonefora.run",
+							"display_url": "gonefora.run",
+							"indices": [
+								90,
+								113
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 531,
+			"friends_count": 278,
+			"listed_count": 18,
+			"created_at": "Tue Sep 11 15:19:08 +0000 2007",
+			"favourites_count": 941,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5417,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/964993178230288384/vi8iEvhL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/964993178230288384/vi8iEvhL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/8812362/1535330015",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Nov 02 12:43:55 +0000 2018",
+			"id": 1058338915969581000,
+			"id_str": "1058338915969581056",
+			"text": "To all my middle aged mates in Lycra: OKAY - this IS impressive 😳 https://t.co/t3CuF6eSL5",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1058338520102850600,
+						"id_str": "1058338520102850560",
+						"indices": [
+							66,
+							89
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1058338520102850560/pu/img/dDjuC_EERMkT-UEU.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1058338520102850560/pu/img/dDjuC_EERMkT-UEU.jpg",
+						"url": "https://t.co/t3CuF6eSL5",
+						"display_url": "pic.twitter.com/t3CuF6eSL5",
+						"expanded_url": "https://twitter.com/cutmaker/status/1058338915969581056/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1058338520102850600,
+						"id_str": "1058338520102850560",
+						"indices": [
+							66,
+							89
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1058338520102850560/pu/img/dDjuC_EERMkT-UEU.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1058338520102850560/pu/img/dDjuC_EERMkT-UEU.jpg",
+						"url": "https://t.co/t3CuF6eSL5",
+						"display_url": "pic.twitter.com/t3CuF6eSL5",
+						"expanded_url": "https://twitter.com/cutmaker/status/1058338915969581056/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								1,
+								1
+							],
+							"duration_millis": 85543,
+							"variants": [
+								{
+									"bitrate": 1280000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1058338520102850560/pu/vid/720x720/Gea1cPEnyrLWMN6d.mp4?tag=5"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1058338520102850560/pu/pl/jiVGJl4vGziGZ6zP.m3u8?tag=5"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1058338520102850560/pu/vid/480x480/Y8OAmUSBVUJJ3CV2.mp4?tag=5"
+								},
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1058338520102850560/pu/vid/240x240/3LsC5av81ERqyH4P.mp4?tag=5"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 18370802,
+				"id_str": "18370802",
+				"name": "Cutmaker",
+				"screen_name": "cutmaker",
+				"location": "",
+				"description": "",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 14446,
+				"friends_count": 334,
+				"listed_count": 40,
+				"created_at": "Thu Dec 25 05:03:04 +0000 2008",
+				"favourites_count": 62,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 5335,
+				"lang": "en-gb",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/885185029734879232/jVIs5T_s_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/885185029734879232/jVIs5T_s_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/18370802/1472018469",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3289,
+			"favorite_count": 9384,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 3289,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Fri Nov 02 10:26:13 +0000 2018",
 		"id": 1058304262189600800,
 		"id_str": "1058304262189600768",
