3f71d0adfd55973ff50da6475a1fc10feb4beb92
Colin Seymour <colin@github.com>
2020-02-16 01:19:06 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5764,
-	"created_at": "Thu Jan 30 01:19:06 +0000 2020",
+	"tweets": 5765,
+	"created_at": "Sun Feb 16 01:19:05 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_02.js
@@ -0,0 +1,251 @@
+Grailbird.data.tweets_2020_02 = 
+[
+	{
+		"created_at": "Sat Feb 15 18:43:39 +0000 2020",
+		"id": 1228751742612967400,
+		"id_str": "1228751742612967424",
+		"text": "This was fantastic!!! I’m still waddling from such a full tummy. 😋😋😋 Thanks @ClaysHyderabadi &amp; @SirenCraftBrew https://t.co/W8LD3T6y6Z",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ClaysHyderabadi",
+					"name": "Clays - Hyderabadi Kitchen",
+					"id": 840146092847116300,
+					"id_str": "840146092847116289",
+					"indices": [
+						76,
+						92
+					]
+				},
+				{
+					"screen_name": "SirenCraftBrew",
+					"name": "Siren Craft Brew",
+					"id": 945733406,
+					"id_str": "945733406",
+					"indices": [
+						99,
+						114
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/W8LD3T6y6Z",
+					"expanded_url": "https://twitter.com/ClaysHyderabadi/status/1224647075616477184",
+					"display_url": "twitter.com/ClaysHyderabad…",
+					"indices": [
+						115,
+						138
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 8812362,
+			"id_str": "8812362",
+			"name": "Lildude Esquire",
+			"screen_name": "lildude",
+			"location": "Everywhere. Wha ha ha ha haaa!",
+			"description": "GitHub developing, beer drinking, running fundi and stay-at-home astronaut. Run stuffs at https://t.co/OlEtj7TstY",
+			"url": "https://t.co/3MoMsEOqye",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3MoMsEOqye",
+							"expanded_url": "https://lildude.co.uk",
+							"display_url": "lildude.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/OlEtj7TstY",
+							"expanded_url": "http://gonefora.run",
+							"display_url": "gonefora.run",
+							"indices": [
+								90,
+								113
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 537,
+			"friends_count": 270,
+			"listed_count": 17,
+			"created_at": "Tue Sep 11 15:19:08 +0000 2007",
+			"favourites_count": 1027,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5680,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/964993178230288384/vi8iEvhL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/964993178230288384/vi8iEvhL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/8812362/1558126627",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1224647075616477200,
+		"quoted_status_id_str": "1224647075616477184",
+		"quoted_status": {
+			"created_at": "Tue Feb 04 10:53:10 +0000 2020",
+			"id": 1224647075616477200,
+			"id_str": "1224647075616477184",
+			"text": "Yay, our first special for 2020 with none other than the amazing @SirenCraftBrew . As always, the dishes will be br… https://t.co/dCu2eP1m6N",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "SirenCraftBrew",
+						"name": "Siren Craft Brew",
+						"id": 945733406,
+						"id_str": "945733406",
+						"indices": [
+							65,
+							80
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/dCu2eP1m6N",
+						"expanded_url": "https://twitter.com/i/web/status/1224647075616477184",
+						"display_url": "twitter.com/i/web/status/1…",
+						"indices": [
+							117,
+							140
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 840146092847116300,
+				"id_str": "840146092847116289",
+				"name": "Clays - Hyderabadi Kitchen",
+				"screen_name": "ClaysHyderabadi",
+				"location": "Reading, England",
+				"description": "A Hyderabadi cuisine based restaurant in Reading - with a combined menu from the Royal kitchens of Nizams of Hyderabad along with food from the streets.",
+				"url": "https://t.co/eBS0oI10tY",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/eBS0oI10tY",
+								"expanded_url": "http://www.clayskitchen.co.uk",
+								"display_url": "clayskitchen.co.uk",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1163,
+				"friends_count": 278,
+				"listed_count": 5,
+				"created_at": "Fri Mar 10 10:23:32 +0000 2017",
+				"favourites_count": 3472,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 1904,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1033861262155898881/c6PDezTj_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1033861262155898881/c6PDezTj_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/840146092847116289/1489964419",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1224379736320811000,
+			"quoted_status_id_str": "1224379736320811009",
+			"retweet_count": 6,
+			"favorite_count": 21,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	}
+]
