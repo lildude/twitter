17071915ca6320eed78c1bdbd343eea3954e73d0
Colin Seymour <colin@github.com>
2021-02-14 01:19:05 +0000

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5879,
-	"created_at": "Sat Feb 06 01:19:05 +0000 2021",
+	"tweets": 5881,
+	"created_at": "Sun Feb 14 01:19:04 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,346 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Sat Feb 13 20:43:30 +0000 2021",
+		"id": 1360691086671437800,
+		"id_str": "1360691086671437830",
+		"text": "RT @JenKatWrites: You know who doesn’t get impostor syndrome? Cats. Not only does every cat know they’re a cat, I think every cat believes…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "JenKatWrites",
+					"name": "Jennifer Adcock, RPG Designer",
+					"id": 1270625196,
+					"id_str": "1270625196",
+					"indices": [
+						3,
+						16
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 8812362,
+			"id_str": "8812362",
+			"name": "Lildude Esquire",
+			"screen_name": "lildude",
+			"location": "Everywhere. Wha ha ha ha haaa!",
+			"description": "GitHub dev, beer drinker, runner & stay-at-home 🧑‍🚀 | Pronoun: Colin/One | https://t.co/OlEtj7TstY | https://t.co/UKMLKK1MEB",
+			"url": "https://t.co/3MoMsEOqye",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3MoMsEOqye",
+							"expanded_url": "https://lildude.co.uk",
+							"display_url": "lildude.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/OlEtj7TstY",
+							"expanded_url": "http://gonefora.run",
+							"display_url": "gonefora.run",
+							"indices": [
+								75,
+								98
+							]
+						},
+						{
+							"url": "https://t.co/UKMLKK1MEB",
+							"expanded_url": "http://colinseymour.co.uk",
+							"display_url": "colinseymour.co.uk",
+							"indices": [
+								101,
+								124
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 526,
+			"friends_count": 275,
+			"listed_count": 15,
+			"created_at": "Tue Sep 11 15:19:08 +0000 2007",
+			"favourites_count": 53,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 196,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1345283359912689665/Rs4N2mwF_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1345283359912689665/Rs4N2mwF_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/8812362/1558126627",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sat Feb 13 03:20:27 +0000 2021",
+			"id": 1360428595647303700,
+			"id_str": "1360428595647303681",
+			"text": "You know who doesn’t get impostor syndrome? Cats. Not only does every cat know they’re a cat, I think every cat bel… https://t.co/ZwfD3hbKYh",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/ZwfD3hbKYh",
+						"expanded_url": "https://twitter.com/i/web/status/1360428595647303681",
+						"display_url": "twitter.com/i/web/status/1…",
+						"indices": [
+							117,
+							140
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1270625196,
+				"id_str": "1270625196",
+				"name": "Jennifer Adcock, RPG Designer",
+				"screen_name": "JenKatWrites",
+				"location": "Buffalo, NY",
+				"description": "RPG designer of Only One Bed, Whispers from the Thousand Moons, and The Price of Coal (upcoming!) ✨ she/her ✨ sorry I never shut up",
+				"url": "https://t.co/EOGGdJD0j3",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/EOGGdJD0j3",
+								"expanded_url": "https://jen-adcock15.itch.io/",
+								"display_url": "jen-adcock15.itch.io",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2926,
+				"friends_count": 661,
+				"listed_count": 26,
+				"created_at": "Fri Mar 15 20:31:32 +0000 2013",
+				"favourites_count": 209829,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 55147,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1321545704666529793/id1kho6s_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1321545704666529793/id1kho6s_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1270625196/1597338618",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 30996,
+			"favorite_count": 112350,
+			"favorited": true,
+			"retweeted": true,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 30996,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en"
+	},
+	{
+		"created_at": "Sat Feb 13 20:04:26 +0000 2021",
+		"id": 1360681257508692000,
+		"id_str": "1360681257508691978",
+		"text": "@keeran 🤗",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "keeran",
+					"name": "keeran",
+					"id": 10075872,
+					"id_str": "10075872",
+					"indices": [
+						0,
+						7
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1360678970342178800,
+		"in_reply_to_status_id_str": "1360678970342178820",
+		"in_reply_to_user_id": 10075872,
+		"in_reply_to_user_id_str": "10075872",
+		"in_reply_to_screen_name": "keeran",
+		"user": {
+			"id": 8812362,
+			"id_str": "8812362",
+			"name": "Lildude Esquire",
+			"screen_name": "lildude",
+			"location": "Everywhere. Wha ha ha ha haaa!",
+			"description": "GitHub dev, beer drinker, runner & stay-at-home 🧑‍🚀 | Pronoun: Colin/One | https://t.co/OlEtj7TstY | https://t.co/UKMLKK1MEB",
+			"url": "https://t.co/3MoMsEOqye",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3MoMsEOqye",
+							"expanded_url": "https://lildude.co.uk",
+							"display_url": "lildude.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/OlEtj7TstY",
+							"expanded_url": "http://gonefora.run",
+							"display_url": "gonefora.run",
+							"indices": [
+								75,
+								98
+							]
+						},
+						{
+							"url": "https://t.co/UKMLKK1MEB",
+							"expanded_url": "http://colinseymour.co.uk",
+							"display_url": "colinseymour.co.uk",
+							"indices": [
+								101,
+								124
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 526,
+			"friends_count": 275,
+			"listed_count": 15,
+			"created_at": "Tue Sep 11 15:19:08 +0000 2007",
+			"favourites_count": 53,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 196,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1345283359912689665/Rs4N2mwF_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1345283359912689665/Rs4N2mwF_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/8812362/1558126627",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "und"
+	},
+	{
 		"created_at": "Fri Feb 05 08:46:00 +0000 2021",
 		"id": 1357611420251271200,
 		"id_str": "1357611420251271168",
